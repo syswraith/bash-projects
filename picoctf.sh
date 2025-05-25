d11094d7b3b2d1703039aad9a50c6131f7d9b97d
syswraith <syswraith@proton.me>
2025-05-25 15:09:10 +0530

    Sun May 25 12:50:18 PM IST 2025

--- picoctf.sh
+++ picoctf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-get_solved_by_diff() {
-  curl -Ls "https://play.picoctf.org/api/users/$1/gym_stats/" | jq -r ".by_difficulty | to_entries[] | \"\(.key) \(.value.solved)\""
+get_stats_by_diff() {
+  curl -Ls "https://play.picoctf.org/api/users/$1/gym_stats/" | jq -r '.by_difficulty | to_entries[] | "\(.key) \(.value.solved) \(.value.available)"'
 }
 
 user1="$1"
@@ -11,38 +11,45 @@ user1_id=$(curl -Ls "https://play.picoctf.org/api/users/$user1/" | jq -r '.id')
 user2_id=$(curl -Ls "https://play.picoctf.org/api/users/$user2/" | jq -r '.id')
 
 printf '\n'
-printf "%-7s   | %9s | %9s | %9s | %-9s\n" "Level" "$user1" "$user2" "Total" "Leader"
+printf "%-7s   | %15s | %15s | %-9s\n" "Level" "$user1" "$user2" "Leader"
 printf '%.s-' {1..60}
 printf '\n'
 
 declare -A user1_solved
+declare -A user1_total
 declare -A user2_solved
+declare -A user2_total
 
-while read diff solved; do
+while read diff solved total; do
   user1_solved[$diff]=$solved
-done < <(get_solved_by_diff "$user1_id")
+  user1_total[$diff]=$total
+done < <(get_stats_by_diff "$user1_id")
 
-while read diff solved; do
+while read diff solved total; do
   user2_solved[$diff]=$solved
-done < <(get_solved_by_diff "$user2_id")
+  user2_total[$diff]=$total
+done < <(get_stats_by_diff "$user2_id")
 
 for diff in 1 2 3; do
-  u1=${user1_solved[$diff]:-0}
-  u2=${user2_solved[$diff]:-0}
-  total=$((u1 + u2))
+  u1_s=${user1_solved[$diff]:-0}
+  u1_t=${user1_total[$diff]:-0}
+  u2_s=${user2_solved[$diff]:-0}
+  u2_t=${user2_total[$diff]:-0}
+
   case $diff in
     1) level="easy" ;;
     2) level="medium" ;;
     3) level="hard" ;;
   esac
-  if (( u1 > u2 )); then
+
+  if (( u1_s > u2_s )); then
     leader="$user1"
-  elif (( u2 > u1 )); then
+  elif (( u2_s > u1_s )); then
     leader="$user2"
   else
     leader="Tie"
   fi
-  printf "%-7s   | %9d | %9d | %9d | %-9s\n" "$level" "$u1" "$u2" "$total" "$leader"
+
+  printf "%-7s   | %7s/%-7s | %7s/%-7s | %-9s\n" "$level" "$u1_s" "$u1_t" "$u2_s" "$u2_t" "$leader"
 done
 printf '\n'
-
